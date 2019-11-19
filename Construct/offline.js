c54ed52a8ace4808c3fe888f69b2cf290daaefde
Moshe-Paz <57709077+Moshe-Paz@users.noreply.github.com>
2019-11-19 16:11:41 +0200

    Add files via upload

---
+++ Construct/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1574172355,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/suncolor.png",
+		"images/mountains1.png",
+		"images/mountains2.png",
+		"images/m3.png",
+		"images/sand.png",
+		"images/sun.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
